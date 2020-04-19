5fbae06f56dc6f2abd181de01782ac3afe94dac9
Husnoro <Husnoro>
2020-04-19 22:51:53 +0500

    добавила Update,  исправила ошибки

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace msql
          PersonDB editor = new PersonDB();
          editor.CheckingConnection();
          editor.OpenConnecting();
-         System.Console.WriteLine("Push *1* --> to add information \nPush *2* --> to see db \nPush *3* --> to select by ID\nYour choise:");
+         System.Console.WriteLine("Push *1* --> to add information \nPush *2* --> to see db \nPush *3* --> to select by ID\nPush *4* --> to update ID \nYour choise:");
          int choise = int.Parse(Console.ReadLine());
          switch (choise)
          {
@@ -23,7 +23,7 @@ namespace msql
             System.Console.WriteLine("Enter Middle Name: ");
             string middleName = Console.ReadLine();
             System.Console.WriteLine("Enter date of birth: ");
-            int dateOfBirth = int.Parse(Console.ReadLine());
+            string dateOfBirth = Console.ReadLine();
                 editor.Insert(lastName,
                               firstName,
                               middleName,
@@ -36,6 +36,19 @@ namespace msql
              System.Console.WriteLine("Enter ID: ");
              int idselect = int.Parse(Console.ReadLine());
              editor.SelectionById(idselect);
+             break;
+             case 4:
+             System.Console.WriteLine("Enter ID yoy want to update: ");
+             int inputId = int.Parse(Console.ReadLine());
+             System.Console.WriteLine("Enter new Last Name: ");
+             string lastName2 = Console.ReadLine();
+             System.Console.WriteLine("Enter new First Name: ");
+             string firstName2 = Console.ReadLine();
+             System.Console.WriteLine("Enter new Middle Name: ");
+             string middleName2 = Console.ReadLine();
+             System.Console.WriteLine("Enter new date of birth: ");
+             string dateOfBirth2 = Console.ReadLine();
+             editor.UpdateById(inputId, lastName2, firstName2, middleName2, dateOfBirth2);
              break;
          }
          editor.CloseConnecting();
--- personDB.cs
+++ personDB.cs
@@ -38,9 +38,9 @@ namespace msql
                 System.Console.WriteLine($"ID:{reader.GetValue(0)}\nLast Name:{reader.GetValue(1)}\nFirst Name:{reader.GetValue(2)}\nMiddle Name:{reader.GetValue(3)}");
             } 
         }
-        public void Insert (string lastName, string firstName, string middleName, int dateOfBirth)
+        public void Insert (string lastName, string firstName, string middleName, string dateOfBirth)
         {
-            string insertingSqlCommand = string.Format($"insert into Person([Last_Name],[First_Name],[Middle_Name], [Birth_date]) values ('{lastName}','{firstName}','{middleName}',{dateOfBirth})");
+            string insertingSqlCommand = string.Format($"insert into Person([Last_Name],[First_Name],[Middle_Name], [Birth_date]) values ('{lastName}','{firstName}','{middleName}', '{dateOfBirth}')");
             
             SqlCommand command = new SqlCommand(insertingSqlCommand,con);
             var result = command.ExecuteNonQuery();
@@ -52,14 +52,27 @@ namespace msql
         public void SelectionById(int idselect)
         {
             string SelectionByIdCommand = string.Format($"select * from Person where Id = {idselect}");
-            SqlCommand command = new SqlCommand(SelectionByIdCommand,con);
+            SqlCommand command = new SqlCommand(SelectionByIdCommand, con);
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
                 System.Console.WriteLine($"ID:{reader.GetValue(0)}\nLast Name:{reader.GetValue(1)}\nFirst Name:{reader.GetValue(2)}\nMiddle Name:{reader.GetValue(3)}");
-            } 
-
+            }
         }
+        public void UpdateById(int inputId, string lastName2, string firstName2, string middleName2, string dateOfBirth2)
+        {
+            string updateIdCommand = string.Format($"update Person set Last_Name = '{lastName2}',First_Name ='{firstName2}',Middle_Name ='{middleName2}',Birth_Date = '{dateOfBirth2}' where Id = {inputId}");
+            SqlCommand command = new SqlCommand(updateIdCommand, con);
+            var result = command.ExecuteNonQuery();
+
+            if(result > 0)
+            {
+               System.Console.WriteLine($"Updated Person with {inputId} Id!"); 
+            }else{
+                System.Console.WriteLine("Такого Id не существует в базе данных!");
+            }
+        }
+        
 
     }
 }
