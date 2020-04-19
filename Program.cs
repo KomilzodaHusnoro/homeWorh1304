1290e60647cbfbb4e208492264f08e27fb43054a
Husnoro <Husnoro>
2020-04-19 17:22:39 +0500

    add sql connection

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace msql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string conString = @"Data sourse= localhost; Initial Catalog= phones; user id=sa; password=Root123.";
+            SqlConnection connection = new SqlConnection();
+        }
+
+    }
+}
